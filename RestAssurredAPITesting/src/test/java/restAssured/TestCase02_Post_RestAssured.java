3bccbdc1bcf355fd4a7ebdc7b74b5ecc27cbde33
Vijaykumar <kulkarni.vijay3@gmail.com>
2019-08-20 20:52:16 +0530

    Post Method Success

--- RestAssurredAPITesting/src/test/java/restAssured/TestCase02_Post_RestAssured.java
+++ RestAssurredAPITesting/src/test/java/restAssured/TestCase02_Post_RestAssured.java
@@ -1,9 +1,12 @@
 package restAssured;
 
 import org.json.simple.JSONObject;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 public class TestCase02_Post_RestAssured {
@@ -11,6 +14,28 @@ public class TestCase02_Post_RestAssured {
 	void RegistrationSuccessful() {
 		RestAssured.baseURI = "http://restapi.demoqa.com/customer";
 		RequestSpecification httpRequest = RestAssured.given();
+		
 		JSONObject requestParms = new JSONObject();
+		requestParms.put("FirstName", "Vijaykumar");
+		requestParms.put("LastName", "Kulkarni");
+		requestParms.put("UserName", "Vijaykumar");
+		requestParms.put("Password", "vijay123");
+		requestParms.put("Email", "vijayk9596@gmail.com");
+		
+		httpRequest.header("content-Type", "application/json");
+		httpRequest.body(requestParms.toJSONString());
+		
+		Response response = httpRequest.request(Method.POST, "/register");
+		
+		String responseBody = response.getBody().asString();
+		
+		System.out.println("Response body: " +responseBody);
+		
+		int statusCode = response.getStatusCode();
+		
+		Assert.assertEquals(statusCode, 201);
+		
+		String successCode = response.jsonPath().get("SuccessCode");
+		Assert.assertEquals(successCode, "OPERATION_SUCCESS");
 	}
 }
